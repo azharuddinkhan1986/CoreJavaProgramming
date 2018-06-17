7be65e537e69e81bcfb386cdb3b57675577387ac
Azharuddin Khan <azhar.sqa@gmail.com>
2018-06-17 10:18:24 +0530

    Reverse String using String Buffer Class

--- src/com/corejava/Programs/ReverseStringUsingStringBufferClass.java
+++ src/com/corejava/Programs/ReverseStringUsingStringBufferClass.java
@@ -5,9 +5,17 @@ public class ReverseStringUsingStringBufferClass {
 	/***
          @Author:Azharuddin Khan
          @Description: This program is used to Reverse a input string using stringbuffer class reverse Method.
+         
+         @return type is StringBuffer
         ***/
 	public StringBuffer reverseStringUsingStringBuffer(String text)
 	{
+		//check for null
+		if(text.equals("")||text.equals(null))
+		{
+			System.out.println("String is null");
+			return null;
+		}
 		StringBuffer sbf=new StringBuffer(text);
 		
 		return sbf.reverse();
@@ -18,9 +26,9 @@ public class ReverseStringUsingStringBufferClass {
 	public static void main(String[] args) {
 		
 		ReverseStringUsingStringBufferClass obj=new ReverseStringUsingStringBufferClass();
-		String text="AzharuddinKhan";
-		StringBuffer result = obj.reverseStringUsingStringBuffer(text);
-		System.out.println(result);
+		StringBuffer x=obj.reverseStringUsingStringBuffer("Azhar");
+		System.out.println(x.toString());
+		
 	}
 
 }
