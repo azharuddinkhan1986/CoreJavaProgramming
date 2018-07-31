daef0f878713ce14f2ead598c74446ba8a78c5fe
AZHARUDDIN KHAN <azhar.sqa@gmail.com>
2018-07-31 14:27:15 +0530

    even Number Program

--- src/com/corejava/Programs/EvenNumberProgram.java
+++ src/com/corejava/Programs/EvenNumberProgram.java
@@ -6,16 +6,6 @@ public class EvenNumberProgram {
 	
 	public static void main(String[] args) {
 		
-		//print even numbers from 1 to 100
-		for(int i=1;i<=100;i++)
-		{
-			if(i%2==0)
-			{
-				System.out.println(i);
-			}
-			
-		}
-		
 		boolean isNumberEven=EvenNumberProgram.isNumberEven(2);
 		System.out.println(isNumberEven);
 		
